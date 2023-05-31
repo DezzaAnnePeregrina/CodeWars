d06e7a1bfe669c9481ca4af12aa36d04fca21508
Dezza Anne Peregrina <116504152+DezzaAnnePeregrina@users.noreply.github.com>
2023-05-31 16:32:10 +0800

    Create sumMixedArray.js

---
+++ 8_Kyu/sumMixedArray.js
@@ -0,0 +1,16 @@
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+
+//Solution 1
+function sumMix(x){
+ return x.map(a => +a).reduce((a,b) => a + b)
+}
+
+
+//Solution 2
+function sumMix(x){
+
+ return x.reduce((a,b) => Number(a) + Number(b) ,0)
+}
