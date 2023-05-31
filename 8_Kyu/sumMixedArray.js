2cd9b93080ab1574e4e72fe9192a43b9d0297838
Dezza Anne Peregrina <116504152+DezzaAnnePeregrina@users.noreply.github.com>
2023-05-31 16:33:29 +0800

    Update sumMixedArray.js

--- 8_Kyu/sumMixedArray.js
+++ 8_Kyu/sumMixedArray.js
@@ -1,6 +1,6 @@
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+/*Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return your answer as a number.
+Return your answer as a number.*/
 
 
 //Solution 1
